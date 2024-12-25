7cdb7e5cf4f2a9435d621954197b2267afcded27
ileisus <96794006+ileisus@users.noreply.github.com>
2024-12-25 05:31:58 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Susie Li on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
