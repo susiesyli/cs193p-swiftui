b81a20bfd2b048ce36ca508be86b789034ce58ca
ileisus <96794006+ileisus@users.noreply.github.com>
2024-12-26 00:04:12 -0500

    ??

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,22 +2,84 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Susie Li on 12/25/24.
+//  Created by Susie Li on 12/21/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["💐", "🌿", "✨", "🪷", "🎄", "🪸", "🌸", "🍀"]
+    @State var cardCount = 4
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
     }
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    func cardCountAdjusters(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjusters(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjusters(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
 }
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+
+        }.onTapGesture {
+            isFaceUp.toggle() // toggle() switches bool
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
